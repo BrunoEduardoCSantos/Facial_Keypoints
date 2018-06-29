d635d0abeff5c04ba821fa3293ff999be6bc7c86
Bruno Santos <bruno.c.santos@ist.utl.pt>
2018-06-29 08:14:59 +0100

    tuned model

--- models.py
+++ models.py
@@ -21,24 +21,23 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 64 output channels/feature maps, 3x3 square convolution kernel
         #(W-F)/S +1 = (224-5)/1 +1 =220 
-        self.conv1 = nn.Conv2d(1, 16, 5)
-        #111X111X32
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        #110X110X32
         self.pool1= nn.MaxPool2d(2,2)
-        #(W-F)/S +1 = (111-3)/1 +1 = 108
-        self.conv2 = nn.Conv2d(16,  32, 3)
-        #55X55X64
+        #(W-F)/S +1 = (110-4)/1 +1 = 108
+        self.conv2 = nn.Conv2d(32,  64, 3)
+        #54X54X64
         self.pool2= nn.MaxPool2d(2,2)
         #(W-F)/S +1 = (54-3)/1 +1 = 52
-        self.conv3 = nn.Conv2d(32,  64, 3)
-        #26X26X64
-        self.pool3 = nn.MaxPool2d(2,2)
+        self.conv3 = nn.Conv2d(64,  128, 3)
         #26X26X128
-        self.conv4 = nn.Conv2d(64,  128, 1)
-       
-      
-        self.fc1= nn.Linear(26*26*128,500)
-        self.fc2= nn.Linear(500,500)
-        self.fc3= nn.Linear(500,136)
+        self.pool3 = nn.MaxPool2d(2,2)
+        #26X26X256
+        self.conv4 = nn.Conv2d(128,  256, 1)
+        self.pool4 = nn.MaxPool2d(2,2)
+        
+        self.fc1= nn.Linear(13*13*256,1000)
+        self.fc2= nn.Linear(1000,136)
 
         self.dropout = nn.Dropout(p=0.2)
      
@@ -58,15 +57,14 @@ class Net(nn.Module):
         x= self.dropout(x)
         x= self.pool3(F.elu(self.conv3(x)))
         x= self.dropout(x)
-        x =F.elu(self.conv4(x))
+        x =self.pool4(F.elu(self.conv4(x)))
         x= self.dropout(x)
         x = x.view(x.size(0), -1)
         x= F.elu(self.fc1(x))
         x= self.dropout(x)
         x= F.elu(self.fc2(x))
         x= self.dropout(x)
-        x= F.elu(self.fc3(x))
-        x= self.dropout(x)
+        
        
         # a modified x, having gone through all the layers of your model, should be returned
         return x
